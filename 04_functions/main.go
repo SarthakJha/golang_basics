822580fbde96bbdba7c199d59d7e6ff3e4bc7f5b
Sarthak Jha <sarthakjha2001@gmail.com>
2020-11-03 20:12:39 +0530

    added: multiple return function

--- 04_functions/main.go
+++ 04_functions/main.go
@@ -8,9 +8,17 @@ func getSum(num1, num2 int) int {
 	return num1 + num2
 }
 
+// function with multiple return value
+func foo2(x int) (int, int) {
+  return x, x+1
+}
 func main() {
 	fmt.Println(greeting("Sarthak"))
 	fmt.Println("sum of 3 and 7 is " + fmt.Sprint(getSum(3, 7)))
+	
+	// calling foo2
+  x,y:=foo2(4)
+  println(x,y) // 4 5
 }
 
 func greeting(name string) string {
